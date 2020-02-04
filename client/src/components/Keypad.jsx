5c0f5687cf33e1a7329637eac30dc086a8e717a2
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-02-03 22:06:09 -0800

    used form to control input component

--- client/src/components/Keypad.jsx
+++ client/src/components/Keypad.jsx
@@ -4,16 +4,39 @@ import React from 'react';
 class Keypad extends React.Component {
 	constructor(props) {
 		super(props);
-	}
+		this.state = {value: ''};
+
+    	this.handleChange = this.handleChange.bind(this);
+    	this.handleSubmit = this.handleSubmit.bind(this);
+	  }
+	  
+	  handleChange(event) {
+		this.setState({value: event.target.value});
+	  }
+	
+	  handleSubmit(event) {
+		this.props.onSubmitCallback(this.state.value);
+		this.setState({
+			value: ""
+		});
+		event.preventDefault();
+	  }
+	
 
 	render() {
 		return (
 			<div>
-				Guess The Number
-				<br />
-					<input style={{border: "2Gpx solid red"}} />
-					<input type="button" value="submit"/>
-				<br />
+				<h3>Guess The Number</h3>
+				<form onSubmit={this.handleSubmit}>
+					<lable>
+						<input maxLength={this.props.maxlength} 
+							   autoFocus value={this.state.value} 
+							   onChange={this.handleChange} 
+							   style={{border: "1px solid gray", fontSize: "18px", width: "100px", margin: "10px"}}
+							   />
+					</lable>
+					<input type="submit" value="Submit" onClick={this.handleSubmit}/>
+				</form>
 			</div>
 		);
 	}
